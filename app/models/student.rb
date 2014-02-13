bbb93de16aed9876344ad46dbf2f0af473a50973
Paul Laskowski <pelaskowski@gmail.com>
2014-02-12 19:50:46 -0800

    added additional func to students.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,14 @@
 class Student < ActiveRecord::Base
+  
+  def combined_info
+    "name: #{full_name} /n"
+    "age: #{age}"
+    "bio: #{bio} /n"
+  end
+    
+  def age_in_60_years
+    age + 60.years
+  end
+  
 end
+
