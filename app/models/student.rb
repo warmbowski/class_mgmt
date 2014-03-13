99e52a682c7cfdf8d288f402f76f73c30dfb89ad
Paul Laskowski <pelaskowski@gmail.com>
2014-03-12 20:52:28 -0700

    remove devise confirmable2

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, #:confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   
   #has_many :courses
   has_many :registrations
